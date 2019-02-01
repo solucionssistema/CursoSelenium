b4f281984d359e63975945ba53aee8bad551c748
Leandro C. Tomassini <solucionssistema@gmail.com>
2019-02-01 00:50:50 -0800

    Vídeo 1 completo sin errorres, abre el navegador Google Chrome y completa usaurio y contraseña con USER, luego va hacia error de login y comprueba si coincide un mensaje con una cadena de texto de una variable.

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -4,14 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Tests {
+	
 	private WebDriver driver;
 	@BeforeMethod
 	public void setUp() {
-		DesiredCapabilities caps = new DesiredCapabilities();
+		DesiredCapabilities caps = new DesiredCapabilities();//lo que esta adentro de un navegador chrome
 		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -29,9 +32,15 @@ public class Tests {
 		driver.findElement(By.name("password")).sendKeys("user");
 		driver.findElement(By.name("login")).click();
 		try {
-			
+			Thread.sleep(5000);
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
+		Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/p/font/b")).getText().contains("Welcome back to"));
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
 	}
 }
